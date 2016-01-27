1a761db5c29474637539516fdfedb732f0973680
Peter S <peter.stanev.bul@gmail.com>
2016-01-27 22:21:49 +0200

    update in layout

--- index.android.js
+++ index.android.js
@@ -12,7 +12,7 @@ var {
   View,
 } = React;
 
-var App = React.createClass({
+var Application = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
@@ -49,4 +49,4 @@ var styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('HealthApp', () => App);
+AppRegistry.registerComponent('Application', () => Application);
